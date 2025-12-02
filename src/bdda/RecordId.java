0fd05e0958e12808f589845103d5515cdd2d5b42
Ali <shouni007@gmail.com>
2025-12-02 14:47:58 +0100

    Mise a jour de la classe Recordid

--- src/bdda/RecordId.java
+++ src/bdda/RecordId.java
@@ -1,19 +1,37 @@
 package bdda;
 
+/**
+ * Identifiant unique d'un record dans la base de données
+ * Composé du PageId de la page et de l'indice du slot dans la page
+ */
 public class RecordId {
-	private PageId pageId;
-	private int slotIdx;
-	
-	public RecordId(PageId pageId, int slotIdx) {
-		this.pageId = pageId;
-		this.slotIdx = slotIdx;
-	}
-	
-	public PageId getPageId() {
-		return pageId;
-	}
-	
-	public int getSlotIdx() {
-		return slotIdx;
-	}
+    
+    private PageId pageId;
+    private int slotIdx;
+    
+    public RecordId(PageId pageId, int slotIdx) {
+        this.pageId = pageId;
+        this.slotIdx = slotIdx;
+    }
+    
+    public PageId getPageId() {
+        return pageId;
+    }
+    
+    public int getSlotIdx() {
+        return slotIdx;
+    }
+    
+    @Override
+    public String toString() {
+        return "RecordId{page=" + pageId + ", slot=" + slotIdx + "}";
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        RecordId other = (RecordId) obj;
+        return slotIdx == other.slotIdx && pageId.equals(other.pageId);
+    }
 }
