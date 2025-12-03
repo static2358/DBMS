ff3a14adb3b98a0db5164dc604153421ecc9ec9f
Ali <shouni007@gmail.com>
2025-12-03 22:18:18 +0100

    Création de la classe SGBD -Ajout du constructeur

--- src/bdda/SGBD.java
+++ src/bdda/SGBD.java
@@ -1,1 +1,37 @@
+package bdda;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Classe principale du SGBD
+ * Point d'entree de l'application
+ * Gere la boucle de commandes et le parsing
+ */
+public class SGBD {
+    
+    private DBConfig config;
+    private DiskManager diskManager;
+    private BufferManager bufferManager;
+    private DBManager dbManager;
+    
+    // Flag pour controler la boucle principale
+    private boolean running;
+    
+    /**
+     * Constructeur
+     * @param config configuration de la base de donnees
+     */
+    public SGBD(DBConfig config) throws IOException {
+        this.config = config;
+        this.diskManager = new DiskManager(config);
+        this.bufferManager = new BufferManager(config, diskManager);
+        this.dbManager = new DBManager(config, diskManager, bufferManager);
+        this.running = true;
+    }
+    
+    
+}
